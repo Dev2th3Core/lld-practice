8e5e74f2dfdf233a75fbc7cf1792faf714436d08
Dev2th3Core <rakshitshinde150@gmail.com>
2025-10-08 20:00:32 +0530

    Added Parking Service

--- parking-lot/C#/C#/Models/Reservation.cs
+++ parking-lot/C#/C#/Models/Reservation.cs
@@ -4,7 +4,7 @@ namespace C_.Models
 {
     class Reservation
     {
-        public Guid ReservationId { get; set; }
+        public Guid ReservationId { get; set; } = Guid.NewGuid();
         public required ParkingSpot Spot { get; set; }
         public required Vehicle Vehicle { get; set; }
         public SpotStatus Status { get; set; } = SpotStatus.Reserved;
